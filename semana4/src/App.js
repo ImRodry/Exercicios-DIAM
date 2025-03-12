1602429926e15dc66b71ae2b48326fb31eb86e17
Rodry <38259440+ImRodry@users.noreply.github.com>
2025-03-12 14:55:43 +0000

    chore: remove random stuff
    chore: rename

--- semana4/src/App.js
+++ semana4/src/App.js
@@ -1,12 +1,11 @@
 import React from "react"
-import Header from "./Components/Header"
-import BarraLateral from "./Components/BarraLateral"
-import Informacoes from "./Components/Informacoes"
-import Horarios from "./Components/Horarios"
-import OutrosFestivais from "./Components/OutrosFestivais"
-import Footer from "./Components/Footer"
-import Artista from "./Components/Artista"
-import Programa from "./Components/Programa"
+import Header from "./components/Header"
+import BarraLateral from "./components/BarraLateral"
+import Informacoes from "./components/Informacoes"
+import Horarios from "./components/Horarios"
+import OutrosFestivais from "./components/OutrosFestivais"
+import Footer from "./components/Footer"
+import Programa from "./components/Programa"
 import "./style.css"
 
 function App() {
@@ -17,19 +16,6 @@ function App() {
 				<Header />
 				<main className="conteudo-principal">
 					<Informacoes />
-					{/* <Artista
-						nome="GNR"
-						imagem="GNR.jpg"
-						estilo="Rock"
-						descricao="GNR (sigla de Grupo Novo Rock) é uma banda portuguesa de pop rock, 
-						formada no Porto, em setembro de 1980, por Alexandre Soares (vocal e guitarra), 
-						Vitor Rua (guitarra) e Tóli César Machado (bateria). O grupo surgiu no período denominado 
-						boom do rock português, mas os elementos da banda consideram que estiveram à margem do 
-						fenómeno, pois quebraram barreiras e criaram uma nova sonoridade em Portugal. 
-						Atualmente a banda é constituída por Tóli César Machado (guitarra, teclas e acordeão),
-						Jorge Romão (baixo) e Rui Reininho (vocal)."
-						datahora="21 de agosto às 21h45"
-					/> */}
 					<Programa />
 					<Horarios />
 					<OutrosFestivais />
